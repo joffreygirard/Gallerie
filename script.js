57f2239948db860f730dfc7d6e242b7eeb7b42e6
Joffrey Girard <joffrey.girard.lpcb@gmail.com>
2020-11-12 13:20:09 +0100

    update fetch

--- script.js
+++ script.js
@@ -2,15 +2,42 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("https://clever-roentgen-a7b6ea.netlify.app/GalleryRepo/images.json")
         .then(function (response) {
             return response.json();
+
         }).then(function (res) {
             console.log(res);
+
+            let images = JSON.parse(res);
+
+            let div_main = document.getElementById("main");
+
+            let container = document.createElement("div");
+            container.classList.add("container", "mt-5", "mb-3");
+
+            let row = document.createElement("div");
+            row.classList.add("row");
+
+            images.forEach(function (image) {
+                let div_image = document.createElement("div");
+                div_image.classList.add("col-sm-6", "col-md-4", "col-lg-3", "p-2");
+
+                let img = document.createElement("img");
+                img.src = image.src;
+                img.classList.add("w-100", "on_hover_clickable");
+
+                div_image.appendChild(img);
+                row.appendChild(div_image);
+            });
+
+            container.appendChild(row);
+            div_main.appendChild(container);
+
         }).catch(function (err) {
             console.log(err);
         });
-})
+});
 
 
-let div_main = document.getElementById("main");
+/*let div_main = document.getElementById("main");
 
 let container = document.createElement("div");
 container.classList.add("container", "mt-5", "mb-3");
@@ -114,4 +141,4 @@ images.forEach(function (image) {
 });
 
 container.appendChild(row);
-div_main.appendChild(container);
+div_main.appendChild(container);*/
