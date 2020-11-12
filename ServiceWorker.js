0545a3245b877c0ff9484eac444b0e98ec750b68
Joffrey Girard <joffrey.girard.lpcb@gmail.com>
2020-11-12 13:57:03 +0100

    add service worker

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,26 @@
-getImages = new Promise((resolve, reject) => {
-
+self.addEventListener('message', event => {
+    this.clients.matchAll()
+        .then(clients => {
+            clients.forEach(client => client.postMessage('Enchanté, je suis le service worker'));
+        });
 });
+
+
+self.addEventListener('fetch', event => {
+    event.RespondWith(new Response('PWA!!!!'));
+});
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(Promise.resolve('Install phase succeed'));
+});
+
+
+/*self.addEventListener('activate', function(event){
+    event.waitUntil(…);
+});*/
+
+
+
 
 
